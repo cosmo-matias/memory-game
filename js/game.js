a200b4dda7774e3d8f3f972623a686c1397e6fd4
Cosmo Matias <110139378+cosmo-matias@users.noreply.github.com>
2025-09-20 22:30:06 -0300

    Implementamos a lógica de clicar na carta e ela virar, e quando encontrar o seu par elas ficarem viradas. Só que até agora só podemos fazer uma única vez.

--- js/game.js
+++ js/game.js
@@ -19,6 +19,43 @@ const creatElement = (tag, className) => {
     return element;
 }
 
+let firstCard = '';
+let secondCard = '';
+
+const checkCards = () => {
+    const firstCharacter = firstCard.getAttribute('data-character');
+    const secondCharacter = secondCard.getAttribute('data-character');
+
+    if (firstCharacter === secondCharacter) {
+
+    } else {
+        setTimeout(() => {
+            firstCard.classList.remove('reveal-card');
+            secondCard.classList.remove('reveal-card');
+
+            firstCard = '';
+            secondCard = '';
+        }, 500);
+    }
+}
+
+const revealCard = ({ target }) => {
+
+    if (target.parentNode.className.includes('reveal-card')) {
+        return;
+    }
+
+    if (firstCard === '') {
+        target.parentNode.classList.add('reveal-card');
+        firstCard = target.parentNode;
+    } else if (secondCard === '') {
+        target.parentNode.classList.add('reveal-card');
+        secondCard = target.parentNode;
+
+        checkCards();
+    }
+}
+
 const creatCard = (character) => {
 
     const card = creatElement('div', 'card');
@@ -30,15 +67,21 @@ const creatCard = (character) => {
     card.appendChild(front);
     card.appendChild(back);
 
+    card.addEventListener('click', revealCard);
+    card.setAttribute('data-character', character);
+
     return card;
 }
 
 const loadGame = () => {
-    characters.forEach((character) => {
 
+    const duplicateCharacters = [ ...characters, ...characters ]; /* com os (...) espalhamos todos os elementos do array characters duas vezes*/
+
+    const shuffleCharacters = duplicateCharacters.sort( () => Math.random() - 0.5 ); /* aqui as cartas serão embaralhadas com o metodo sort, porém esse metodo precisa de uma função, nesse caso usamos a função Math.rondom() - 0.5 para gerar um número menor que zero e maior que zero, porém menor que 1. */
+
+    shuffleCharacters.forEach((character) => {
         const card = creatCard(character);
         grid.appendChild(card);
-
     })
 }
 
